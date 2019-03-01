a8cd4a59aa264a190aea9aab4cade145b4b8a5f8
Nicholas <englishnicholas@yahoo.com>
2019-03-01 13:05:08 -0500

    More JavaDoc comments and JavaDoc documents.

--- src/topdown/State.java
+++ src/topdown/State.java
@@ -1,12 +1,37 @@
 package topdown;
 
+/**
+ * Creates the different states available to be used to say what state the
+ * game currently is in.
+ * 
+ * @author Issac Jimenez
+ * @author Nicholas English
+ * @author Suman Gurung
+ * @version 1.0
+ */
 public enum State {
+	
+	/** Currently in the start menu state. **/
     Menu(),
+    
+    /** Currently in the game state. **/
     Game(),
+    
+    /** The game is in the options menu state. **/
     OptionsMenu(),
+    
+    /** The game is currently paused state. **/
     PauseMenu(),
+    
+    /** The game is showing a menu state. **/
     ControlsMenu(),
+    
+    /** The game is showing credits state. **/
     Credits(),
+    
+    /** The game has been lost state. **/
     GameOver(),
+    
+    /** The game has been won state. **/
     GameWon()
 }
--- src/topdown/Window.java
+++ src/topdown/Window.java
@@ -3,11 +3,27 @@ package topdown;
 import java.awt.Dimension;
 import javax.swing.JFrame;
 
+/**
+ * Used to create a window for the game screen to be played in.
+ * 
+ * @author Issac Jimenez
+ * @author Nicholas English
+ * @author Suman Gurung
+ * @version 1.0
+ */
 public class Window {
 
 	/** Holds the frame for the game screen. **/
     private static JFrame frame;
 
+    /**
+     * Creates a window for the game screen to be played in.
+     * 
+     * @param width The integer value for the width of the window.
+     * @param height The integer value for the height of the window.
+     * @param title The title for the window as a string.
+     * @param game Holds the current game object.
+     */
     public Window(final int width, final int height, 
     		final String title, final Game game) {
 
